74ac4936612dd0b77743622f1ebe7622241538ea
Agneya T <agneyat2@gmail.com>
2024-11-15 11:33:54 -0500

    css changes

--- src/app/neweditor/page.tsx
+++ src/app/neweditor/page.tsx
@@ -170,25 +170,21 @@ const Editor = () => {
   };
 
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
+    <div className="flex h-screen">
       {/* Sidebar */}
-      <div style={{ width: '200px', borderRight: '1px solid #ccc', padding: '10px' }}>
-        <button onClick={addFile}>Add File</button>
+      <div className="min-w-[200px] max-w-[300px] border-r border-gray-300 p-2.5 overflow-y-auto">
         <ul>
           {Object.keys(files).map((fileName) => (
-            <li key={fileName}>
-              <span
-                style={{
-                  cursor: 'pointer',
-                  fontWeight: activeFile === fileName ? 'bold' : 'normal',
-                }}
+            <li key={fileName} className='flex flex-row space-x-2 w-full mt-2'>
+              <button
+                className={`content-center cursor-pointer ${activeFile === fileName ? 'font-bold' : 'font-normal'
+                  }`}
                 onClick={() => setActiveFile(fileName)}
               >
                 {fileName}
-              </span>
+              </button>
               {fileName !== 'Main.java' && (
-                <>
-                  <button onClick={() => removeFile(fileName)}>Delete</button>
+                <div className='ml-auto space-x-2 flex w-max'>
                   <button
                     onClick={() => {
                       const newFileName = prompt('Enter new file name', fileName);
@@ -196,18 +192,33 @@ const Editor = () => {
                         renameFile(fileName, newFileName);
                       }
                     }}
+                    className='bg-stone-400 rounded-md p-1'
                   >
-                    Rename
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-6 h-6'>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+
                   </button>
-                </>
+                  <button onClick={() => removeFile(fileName)} className='bg-red-400 rounded-md p-1'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-6 h-6'>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+
+                  </button>
+
+                </div>
               )}
             </li>
           ))}
         </ul>
-        <button onClick={runCode} disabled={!cheerpjLoaded}>
-          Run
-        </button>
-        {!cheerpjLoaded && <div>Loading CheerpJ...</div>}
+        <div className='flex flex-col space-y-2 pt-2'>
+          <button className="rounded-md py-1 bg-stone-400" onClick={addFile}>Add File</button>
+          <button className="rounded-md py-1 bg-red-400" onClick={runCode} disabled={!cheerpjLoaded}>Run Main.java</button>
+          {!cheerpjLoaded && <div>Loading CheerpJ...</div>}
+          {/* <button onClick={() => setShowDisplay((prev) => !prev)}>
+            {showDisplay ? 'Hide Display' : 'Show Display'}
+          </button> */}
+        </div>
       </div>
       {/* Editor and Output */}
       <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
@@ -229,6 +240,7 @@ const Editor = () => {
           <div style={{ width: '400px', position: 'relative' }}>
             <div ref={displayRef} style={{ width: '100%', height: '100%' }} />
           </div>
+          {/* {showDisplay && <div ref={displayRef} />} */}
         </div>
       </div>
     </div>
